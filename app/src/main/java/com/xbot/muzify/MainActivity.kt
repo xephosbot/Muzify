f5cc9befae673fada166d05e991a1ce823495533
xephosbot <xephosbot@gmail.com>
2023-05-03 20:11:57 +0300

    Initial commin

---
+++ app/src/main/java/com/xbot/muzify/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.xbot.muzify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContentView(R.layout.activity_main)
+    }
+}
